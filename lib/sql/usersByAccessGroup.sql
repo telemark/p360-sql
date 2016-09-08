58dca04c7a24a0f5d108ab357e29df6f4200a277
Jonas Enge <jonas.enge@t-fk.no>
2016-09-08 11:31:10 +0200

    Removes domain

--- lib/sql/usersByAccessGroup.sql
+++ lib/sql/usersByAccessGroup.sql
@@ -7,7 +7,7 @@ Set @LanguageCode = 'NOR';
 
 SELECT AccessGroup.uag_code as 'accessGroup',
 Contact.ct_name2 + ' ' + Contact.ct_name3 as name,
-Usr.us_id as 'userId' from us_user as Usr
+REPLACE(Usr.us_id, 'login\', '') as 'userId' from us_user as Usr
 
 LEFT JOIN ugm_groupmember on ugm_groupmember.ugm_ct_recno = Usr.us_ct_recno
 LEFT JOIN uag_accessgroup as AccessGroup on AccessGroup.uag_recno = ugm_groupmember.ugm_uag_recno
